ecdf65e5ff5b78e0d46a526a347fad8be96a6183
abhinaw998 <abhinaw.kumar998@gmail.com>
2024-02-23 04:39:01 +0530

    formated

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 Vue.createApp({
-    // data:function(){
+  // data:function(){
 
-    // }
-    // this is the one of the pre define property of vue js wchich is use to  get and pas the value in shtml
-    data: () => {
-        return {
-            goals: [],
-            enteredValue: ""
-        }; s
+  // }
+  // this is the one of the pre define property of vue js wchich is use to  get and pas the value in shtml
+  data: () => {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+    s;
+  },
+  // this is the one of the pre define property of vue js wchich is use to define the method is used in html
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    // this is the one of the pre define property of vue js wchich is use to define the method is used in html
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue='';
-        }
-    }
-}).mount("#app")
+  },
+}).mount("#app");
